faf335749e299ac422a6b87e4506c5a7c3840b56
stonegrape <1622313352@qq.com>
2020-12-28 21:48:26 +0800

    React app:component

---
+++ src/components/Todo.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+
+/*
+Not:Components must always return something. 
+If at any point in the future you try to render a component that does not return anything, 
+React will display an error in your browser.
+*/
+export default function Todo(props) {
+    return (
+    <li className="todo stack-small">
+        <div className="c-cb">
+            <input id={props.id} type="checkbox" defaultChecked={props.completed} />
+            <label className="todo-label" htmlFor="todo-0">
+            {props.name}
+            </label>
+        </div>
+        <div className="btn-group">
+            <button type="button" className="btn">
+            Edit <span className="visually-hidden">{props.name}</span>
+            </button>
+            <button type="button" className="btn btn__danger">
+            Delete <span className="visually-hidden">{props.name}</span>
+            </button>
+        </div>
+    </li>
+    );
+}
